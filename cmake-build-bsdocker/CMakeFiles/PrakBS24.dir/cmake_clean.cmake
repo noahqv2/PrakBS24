2c00eafa9af15ab1e40890974293ae28d9a9093d
Noah <noah_osato.asemota@smail.th-koeln.de>
2024-04-25 13:32:52 +0200

    first commit :D

---
+++ cmake-build-bsdocker/CMakeFiles/PrakBS24.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PrakBS24.dir/main.c.o"
+  "PrakBS24.pdb"
+  "PrakBS24"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/PrakBS24.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
